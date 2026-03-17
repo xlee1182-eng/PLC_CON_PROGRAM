4d265b64bdfddd391480c1cf3662a7cf81d3bf68
leex <xiong.li@mobyus.com>
2026-03-17 13:03:34 +0900

    up 2026.03.17

--- app/routes/api/RootApi.py
+++ app/routes/api/RootApi.py
@@ -3,8 +3,7 @@ from loguru import logger
 ## functions
 import app.functions.CommonFunction as __FUNCTION_COMMON
 
-import app.db.DBManager as __DB_MANAGER
-db = __DB_MANAGER.DBManager()
+
 
 router = APIRouter()
 
@@ -34,69 +33,3 @@ def health():
   except Exception as e:
                 logger.error(f"API route fatal error: {e}")
 
-@router.get('/select')
-def select():
-  try:
-    rows = db.EXECUTE(
-    "MES",
-    "GET",
-    "SELECT * FROM MES_TABLE WHERE ID = :id",
-    {"id":1}
-    )
-
-    print(rows)
-
-    data = rows
-
-    result = data
-
-    return result
-  except Exception as e:
-                logger.error(f"API route fatal error: {e}")
-@router.get('/insert')
-def insert():
-  try:
-    rows = db.EXECUTE(
-    "MES",
-    "SET",
-    "INSERT INTO TEST_TABLE(ID,NAME) VALUES(:id,:name)",
-    {"id":10,"name":"TEST"}
-)
-
-    print(rows)
-
-    data = rows
-
-    result = data
-
-    return result
-  except Exception as e:
-                logger.error(f"API route fatal error: {e}")
-@router.get('/reconnect')
-def reconnect():
-  try:
-    db.RECONNECT("MES")
-    rows = "reconnected"
-    print(rows)
-
-    data = rows
-
-    result = data
-
-    return result
-  except Exception as e:
-                logger.error(f"API route fatal error: {e}")
-@router.get('/create_cursor')
-def create_cursor():
-  try:
-    cursor = db.CREATECURSOR("MES")
-    rows = "cursor created"
-    print(rows)
-
-    data = rows
-
-    result = data
-
-    return result
-  except Exception as e:
-                logger.error(f"API route fatal error: {e}")
